57e664b246fbcca9c0036e935379e9de1ff586f3
Janishnehyan03 <nehyanjanish@gmail.com>
2024-10-11 17:10:09 +0530

    fix bugs

--- src/Axios.ts
+++ src/Axios.ts
@@ -1,8 +1,8 @@
 import axios from "axios";
 // Create an instance of Axios
 const Axios = axios.create({
-  // baseURL: "https://huzoor-server.vercel.app/api", // Set base URL for all requests
-  baseURL: "http://localhost:5000/api", // Set base URL for all requests
+  baseURL: "https://huzoor-server.vercel.app/api", // Set base URL for all requests
+  // baseURL: "http://localhost:5000/api", // Set base URL for all requests
   timeout: 5000, // Set timeout for requests (in milliseconds)
   headers: {
     "Content-Type": "application/json", // Set default Content-Type header
